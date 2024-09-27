7f6e3f22e41b349ebfdfd033a36263b561c44bd5
eungyu04 <eunkyu7889@naver.com>
2024-09-27 23:14:16 +0900

    test0927

---
+++ Invaders-SDP-master (1)/Invaders-SDP-master/src/entity/ItemOnetime.java
@@ -0,0 +1,33 @@
+package entity;
+
+import java.awt.*;
+
+public class ItemOnetime extends Entity {
+
+    private int speed;
+
+    /** Sprite types. */
+    public static enum SpriteType {
+        /** attactspeed item */
+        ItemAttackSpeed,
+        /** barrier item */
+        ItemBarrier,
+        /** recovery item */
+        ItemRecovery,
+        /** Bomb item */
+        ItemBomb
+    };
+
+    /**
+     * Public constructor.
+     */
+    public ItemOnetime(final int positionX, final int positionY, final int speed) {
+        super(positionX, positionY, 3 * 3, 3 * 3, Color.magenta);
+
+        this.speed = speed;
+    }
+
+
+
+
+}
